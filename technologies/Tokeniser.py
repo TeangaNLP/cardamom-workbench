d7f46ab36f42e7ad688870cf87d87b8a287099f5
admd9 <admd911@hotmail.com>
2022-05-05 23:20:36 +0100

    Updated tokeniser output to meet standards discussed in most recent meeting. Allows pass-through of log data from workbench. General tokenisation is now possible where no language is selected.

--- technologies/Tokeniser.py
+++ technologies/Tokeniser.py
@@ -1,11 +1,16 @@
 from nltk.tokenize import word_tokenize
+from datetime import date, datetime
 
 
-def cardamom_tokenise(string, text_language, reserved_toks=None):
+def cardamom_tokenise(string, log_data, text_language=None, reserved_toks=None):
     """Tokenises a string of text and returns a list containing data for each token in the in a dictionary:
 
-       [{'token': 'str', 'start': int, 'end': int, 'reserved_token': bool,
-       'text_language': 'str', 'token_language': 'str'}, ...]
+       [{'type': 'token', 'start': int, 'end': int, 'text_language': str, 'token_language': str, 'data': dict},
+       ...]
+
+       Each token will contain a 'data' key, which will allow the pass-through of log data from the workbench:
+
+       {... 'data': {'user_id': int, 'time': str, 'date': str, 'reserved': bool}}
 
        A list of reserved tokens which have been manually annotated can be supplied as an argument, these will be
        passed through without being affected."""
@@ -22,15 +27,17 @@ def cardamom_tokenise(string, text_language, reserved_toks=None):
         # Check that reserved tokens are in the correct format, if not raise error with informative message.
         if not isinstance(reserved_toks, list):
             raise RuntimeError(f"\nExpected array of reserved tokens where each token is of type 'dict',\neg. "
-                               f"[{{'token': 'Hello', 'start': 0, 'end': 5, 'reserved_token': True, "
-                               f"'text_language': 'english', 'token_language': 'english'}}, {{...}} ...].\n"
-                               f"Instead, got {type(reserved_toks).__name__} class entry: {reserved_toks}.")
+                               f"[{{'type': 'token', 'start': 0, 'end': 5, 'text_language': 'english',"
+                               f"'token_language': 'english', 'data': {{'user_id': int, 'time': str, 'date': str, "
+                               f"'reserved': bool}}}}, {{...}}, ...]\nInstead, got {type(reserved_toks).__name__} "
+                               f"class entry:\n{reserved_toks}.")
         for indset in reserved_toks:
             if not isinstance(indset, dict):
                 raise RuntimeError(f"\nExpected array of reserved tokens where each token is of type 'dict',\neg. "
-                                   f"[{{'token': 'Hello', 'start': 0, 'end': 5, 'reserved_token': True, "
-                                   f"'text_language': 'english', 'token_language': 'english'}}, {{...}} ...].\n"
-                                   f"Problem found in '{type(indset).__name__}' class entry: {indset}.")
+                                   f"[{{'type': 'token', 'start': 0, 'end': 5, 'text_language': 'english',"
+                                   f"'token_language': 'english', 'data': {{'user_id': int, 'time': str, 'date': str, "
+                                   f"'reserved': bool}}}}, {{...}}, ...]\nProblem found in '{type(indset).__name__}' "
+                                   f"class entry: {indset}.")
         stringlist = list()
         current_index = 0
         for indset in reserved_toks:
@@ -38,7 +45,7 @@ def cardamom_tokenise(string, text_language, reserved_toks=None):
             if reserved_toks.count(indset) > 1:
                 raise RuntimeError(f"Duplicate token found in reserved tokens' list: {indset}")
             # Check that reserved tokens contain the correct key-value pairs, if not raise error.
-            checklist = ["token", "start", "end", "reserved_token", "text_language", "token_language"]
+            checklist = ["type", "start", "end", "text_language", "token_language", "data"]
             if not all(data_type in checklist for data_type in indset):
                 problem_keys = [key for key in indset if key not in checklist]
                 if len(problem_keys) == 1:
@@ -46,9 +53,8 @@ def cardamom_tokenise(string, text_language, reserved_toks=None):
                     plurality = ""
                 else:
                     plurality = "s"
-                raise RuntimeError(f"\nExpected array of reserved tokens where each token is of type 'dict',\neg. "
-                                   f"[{{'token': 'Hello', 'start': 0, 'end': 5, 'reserved_token': True, "
-                                   f"'text_language': 'english', 'token_language': 'english'}}, {{...}} ...].\n"
+                raise RuntimeError(f"\nExpected array of reserved tokens where each token is of type 'dict',\n"
+                                   f"Each dict should contain the keys: {checklist}\n"
                                    f"Problem found in key{plurality}: {problem_keys}.")
             if not all(data_type in indset for data_type in checklist):
                 problem_keys = [key for key in checklist if key not in indset]
@@ -57,14 +63,17 @@ def cardamom_tokenise(string, text_language, reserved_toks=None):
                     plurality = ""
                 else:
                     plurality = "s"
-                raise RuntimeError(f"\nExpected array of reserved tokens where each token is of type 'dict',\neg. "
-                                   f"[{{'token': 'Hello', 'start': 0, 'end': 5, reserved_token': True, "
-                                   f"'text_language': 'english', 'token_language': 'english'}}, {{...}} ...].\n"
+                raise RuntimeError(f"\nExpected array of reserved tokens where each token is of type 'dict',\n"
+                                   f"Each dict should contain the keys: {checklist}\n"
                                    f"Key{plurality}, {problem_keys}, missing.")
-            tok_form = indset.get("token")
+            tok_type = indset.get("type")
             # Check that the value for the reserved token's form is a string.
-            if not isinstance(tok_form, str):
-                raise RuntimeError(f"Expected value 'str' for key, 'token'. Got {type(tok_form).__name__}\n{indset}")
+            if not isinstance(tok_type, str):
+                raise RuntimeError(f"Expected value of type 'str' for key, 'type'. "
+                                   f"Got value of type '{type(tok_type).__name__}':\n{indset}")
+            elif tok_type != "token":
+                raise RuntimeError(f"Expected value 'token' for key, 'type'. "
+                                   f"Got value '{tok_type}':\n{indset}")
             start = indset.get("start")
             end = indset.get("end")
             # Check that values of start and end indices for reserved tokens are integers. If not, raise error.
@@ -75,14 +84,14 @@ def cardamom_tokenise(string, text_language, reserved_toks=None):
                 elif not isinstance(end, int):
                     raise RuntimeError(f"Expected value of class 'int' for key, 'end'. Got class "
                                        f"'{type(end).__name__}':\n{indset}")
-            reserved = indset.get("reserved_token")
+            reserved = indset.get("data").get("reserved")
             # Check that all values for identifying reserved tokens are Boolean.
             if not isinstance(reserved, bool):
-                raise RuntimeError(f"Expected of class 'bool' for key, 'reserved_token'. Got class "
+                raise RuntimeError(f"Expected of class 'bool' for key, 'reserved'. Got class "
                                    f"'{type(reserved).__name__}':\n{indset}")
             # Check that all tokens reserved tokens supplied are identified as reserved tokens.
             if not reserved:
-                raise RuntimeError("Expected Boolean value True for key, 'reserved_token'. Got value False")
+                raise RuntimeError("Expected Boolean value True for key, 'reserved'. Got value False")
             # Check that reserved tokens are in order of occurrence and do not overlap in the string.
             # If they are out of order or overlap, raise an error.
             if start < current_index:
@@ -90,11 +99,6 @@ def cardamom_tokenise(string, text_language, reserved_toks=None):
                                    f"({start}) of token, {indset}, precedes end index of preceding token "
                                    f"({current_index}).")
             string_to_tok = string[current_index:start]
-            reserved_token = string[start:end]
-            # Raise an error if the reserved token does not match the text in the string between the provided indices.
-            if tok_form != reserved_token:
-                raise RuntimeError(f'Reserved token, "{tok_form}", did not match text in string, "{reserved_token}", '
-                                   f'from index {start} to index {end}.')
             stringlist.extend([("^tokenise", string_to_tok), ("^reserved", indset)])
             current_index = end
         string_to_end = string[current_index:]
@@ -106,27 +110,35 @@ def cardamom_tokenise(string, text_language, reserved_toks=None):
 
     # Tokenise substrings which are tagged for tokenisation and add resulting tokens to the token list.
     # Add reserved tokens to the token list without changes.
-    if text_language in nltk_langs:
-        token_list = list()
-        for substring in stringlist:
-            tok_tag = substring[0]
-            tok_string = substring[1]
-            if tok_tag == "^tokenise":
-                token_list.extend(word_tokenize(tok_string, text_language))
-            elif tok_tag == "^reserved":
-                token_list.append(tok_string)
-    else:
-        raise RuntimeError(f'Could not tokenise text. Check language, "{text_language}", is supported by tokeniser.')
+    token_list = list()
+    for substring in stringlist:
+        tok_tag = substring[0]
+        tok_string = substring[1]
+        if tok_tag == "^tokenise":
+            if text_language:
+                if text_language in nltk_langs:
+                    token_list.extend(word_tokenize(tok_string, text_language))
+                else:
+                    raise RuntimeError(f'Could not tokenise text. '
+                                       f'Check language, "{text_language}", is supported by tokeniser.')
+            else:
+                token_list.extend(word_tokenize(tok_string))
+        elif tok_tag == "^reserved":
+            token_list.append(tok_string)
 
     # Iterate through the string to find the indices of each token, add these to a list for output.
     indexed_tokens = list()
     current_index = 0
+    user_id = log_data.get("user_id")
+    user_time = log_data.get("time")
+    user_date = log_data.get("date")
     for token in token_list:
         # For tokens which have just been tokenised above, create token data for them and add this to indexed list.
         if isinstance(token, str):
             tok_index = string[current_index:].find(token) + current_index
-            tok_dict = {"token": token, "start": tok_index, "end": tok_index + len(token),
-                        "reserved_token": False, "text_language": text_language, "token_language": text_language}
+            tok_dict = {"type": "token", "start": tok_index, "end": tok_index + len(token),
+                        "text_language": text_language, "token_language": text_language, "data":
+                            {'user_id': user_id, 'time': user_time, 'date': user_date, 'reserved': False}}
         # For reserved tokens, make no changes to token data and add it to indexed list.
         elif isinstance(token, dict):
             tok_dict = token
@@ -151,13 +163,15 @@ def cardamom_tokenise(string, text_language, reserved_toks=None):
 #               "als „überzeugter Atheist“, 44 Prozent nannten sich „nicht-religiös“ und 47 Prozent gaben an, eine " \
 #               "religiöse Person zu sein."
 #
-#     no_tok = [{"token": "very much", "start": 39, "end": 48, "reserved_token": True,
-#                "text_language": "english", "token_language": "english"},
-#               {"token": "time-waste", "start": 141, "end": 151, "reserved_token": True,
-#                "text_language": "english", "token_language": "english"},
-#               {"token": "Críoch", "start": 153, "end": 159, "reserved_token": True,
-#                "text_language": "english", "token_language": "irish"}]
+#     res_toks = [{"type": "token", "start": 39, "end": 48, "text_language": "english", "token_language": "english",
+#                  "data": {"user_id": 3, "time": "13:53:35", "date": "02/04/2022", "reserved": True}},
+#                 {"type": "token", "start": 141, "end": 151, "text_language": "english", "token_language": "english",
+#                  "data": {"user_id": 3, "time": "13:53:35", "date": "02/04/2022", "reserved": True}},
+#                 {"type": "token", "start": 153, "end": 159, "text_language": "english", "token_language": "irish",
+#                  "data": {"user_id": 2, "time": "09:22:18", "date": "23/04/2022", "reserved": True}}]
 #
-#     # print(cardamom_tokenise(test_en, "english"))
-#     # print(cardamom_tokenise(test_de, "german"))
-#     print(cardamom_tokenise(test_en, "english", reserved_toks=no_tok))
+#     test_data = {"user_id": 1, "time": datetime.now().strftime("%H:%M:%S"), "date": date.today().strftime("%d/%m/%Y")}
+#
+#     # print(cardamom_tokenise(test_en, test_data, "english"))
+#     # print(cardamom_tokenise(test_de, test_data, "german"))
+#     print(cardamom_tokenise(test_en, test_data, "english", res_toks))
