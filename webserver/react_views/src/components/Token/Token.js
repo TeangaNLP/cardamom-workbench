3e82fa21a9a81ef8de3054467974ccf39d3ac371
ELITA04 <elita041999@gmail.com>
2022-07-12 14:15:14 +0100

    Added quick fix for master

--- webserver/react_views/src/components/Token/Token.js
+++ webserver/react_views/src/components/Token/Token.js
@@ -2,24 +2,37 @@ import { useState, useRef } from "react";
 import useClickOutside from "../utils/useClickOutside";
 import "./Token.css";
 
-export default function Token({ downHandler, upHandler, deselectHandler, token, value }) {
+export default function Token({
+  downHandler,
+  upHandler,
+  deselectHandler,
+  token,
+  value,
+}) {
+  // Custom hook that notifies when clicked outside this component.
+  const ref = useRef();
+  useClickOutside(ref, null, () => {
+    deselectHandler();
+  });
 
-    // Custom hook that notifies when clicked outside this component.
-    const ref = useRef();
-    useClickOutside(ref, null, () => {
-        deselectHandler();
-    });
+  const handleMouseDown = () => {
+    downHandler(token.start_index);
+  };
 
+  const handleMouseUp = () => {
+    upHandler(token.start_index);
+  };
 
-    const handleMouseDown = () => {
-        downHandler(token.start_index);
-    }
+  console.log(token.type);
 
-    const handleMouseUp = () => {
-        upHandler(token.start_index);
-    }
-
-    return (
-        <span ref={ref} className={`token ${token.type}`} onMouseDown={handleMouseDown} onMouseUp={handleMouseUp}>{token.type == "manual" ? value.replace(" ", "⎵") : value}</span>
-    );
+  return (
+    <span
+      ref={ref}
+      className={`token ${token.type}`}
+      onMouseDown={handleMouseDown}
+      onMouseUp={handleMouseUp}
+    >
+      {token.type == "manual" ? value.replace(" ", "⎵") : value}
+    </span>
+  );
 }
