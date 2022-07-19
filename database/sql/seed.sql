adf90b681972f0d731fe5828f7e22d1ea4f924f1
ELITA04 <elita041999@gmail.com>
2022-07-19 16:51:31 +0100

    Minor change

--- database/sql/seed.sql
+++ database/sql/seed.sql
@@ -139,7 +139,7 @@ INSERT INTO uploaded_files (name, content, user_id, language_id) VALUES ('a.txt'
 
 INSERT INTO uploaded_files (name, content, user_id, language_id) VALUES ('b.txt', '.i. biuusa ocirbáig darfarcennsi frimaccidóndu\n\n.i. niarformut fribsi asbiursa inso arropad maith limsa labrad ilbelre dúibsi\n\n.i. isipersin crist dagníusa sin\n\n.i. ó domanicc foirbthetu ní denim gnímu macthi act rísam nem bimmi æcni et bimmi foirbthi uili\n\n.i. isocprecept soscéli attó\n\n.i. ished inso noguidimm .i. conducaid etargne ṅ dǽ et conaroib temel innatol domunde tarrosc fornanme\n\n.i. hore nondobmolorsa et nom móidim indib\n\n.i. amal nondafrecṅdirccsa\n\n.i. is inse ṅduit nitú nodnai(l) acht ishé not ail\n\n.i. madarlóg pridchasa .i. armetiuth et mothoschith nímbia fochricc dar hési moprecepte\n\n.i. coníarimse peccad libsi uili ɫ. aratartsa fortacht dúibsi arnap trom fuirib fornóinur\n\n.i. cote mothorbese dúib madamne labrar\n\n.i. nihed notbeir ínem ciabaloingthech\n\nAcht nammáa issamlid istorbe són co etercerta anasbera et conrucca inætarcne cáich\n\n.i. léic uáit innabiada mílsi ettomil innahí siu dommeil do chenél arnáphé som conéit détso\n\n	.i. isamlid dorígeni dia corp duini ó ilballaib\n\n	.i. act basamlid dúib cid immeícndarcus', 1, 2);
 
-INSERT INTO annotations (token, reserved_token, start_index, end_index, token_language_id, type, uploaded_file_id) VALUES ('Lorem', TRUE, 0, 4, 1, 'manual', 1);
+INSERT INTO tokens (token, reserved_token, start_index, end_index, token_language_id, type, uploaded_file_id) VALUES ('Lorem', TRUE, 0, 4, 1, 'manual', 1);
 
 INSERT INTO tokens (token, reserved_token, start_index, end_index, token_language_id, type, uploaded_file_id) VALUES ('ipsum', TRUE, 6, 11, 1, 'manual', 1);
 
