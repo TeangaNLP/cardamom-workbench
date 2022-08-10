efbfc39a1ebe0c7ef8a5439d9553e798976e1f0f
berstearns <bernardo.stearns@gmail.com>
2022-08-10 12:07:59 +0100

    adding cors to flask to support isolated react app

--- webserver/flask_app.py
+++ webserver/flask_app.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from api import api
 from views import views
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 app.register_blueprint(api, url_prefix='/api')
 app.register_blueprint(views)
 
