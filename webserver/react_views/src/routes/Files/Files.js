473da004d6750caff221954fc7a2052c327e9f2a
Alamgir Qazi <alamgirmunirqazi@gmail.com>
2024-05-20 17:33:02 +0100

    fixed table design

--- webserver/react_views/src/routes/Files/Files.js
+++ webserver/react_views/src/routes/Files/Files.js
@@ -23,6 +23,9 @@ import {
   Paper,
 } from "@mui/material";
 import { useNavigate } from "react-router-dom";
+import { styled } from "@mui/material/styles";
+
+import { tableCellClasses } from "@mui/material/TableCell";
 
 export default function Files({ user }) {
   const [documentsList, setDocuments] = React.useState([]);
@@ -58,18 +61,80 @@ export default function Files({ user }) {
     }
   }, []);
 
+  const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: theme.palette.primary.main,
+      color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+    },
+  }));
+
+  const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    "&:nth-of-type(odd)": {
+      backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    "&:last-child td, &:last-child th": {
+      border: 0,
+    },
+  }));
+
   return (
     <div>
       <NavBar />
       <Box
         sx={{
-          marginTop: "64px",
+          marginTop: "24px",
           padding: "25px",
           display: "flex",
           justifyContent: "center",
         }}
       >
         <TableContainer component={Paper}>
+          <Table sx={{ minWidth: 700 }} aria-label="customized table">
+            <TableHead>
+              <TableRow>
+                <StyledTableCell sx={{ width: "80px" }}>
+                  File ID
+                </StyledTableCell>
+                <StyledTableCell align="left">File Name</StyledTableCell>
+                <StyledTableCell align="left">Content</StyledTableCell>
+                <StyledTableCell sx={{ width: "180px" }} align="left">
+                  Action
+                </StyledTableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {documentsList.map((row) => (
+                <StyledTableRow key={row.file_id}>
+                  <StyledTableCell component="th" scope="row">
+                    {row.file_id}
+                  </StyledTableCell>
+                  <StyledTableCell align="left">{row.filename}</StyledTableCell>
+                  <StyledTableCell align="left">
+                    {" "}
+                    {row.content.length > 250
+                      ? row.content.slice(0, 250) + "..."
+                      : row.content}
+                  </StyledTableCell>
+                  <StyledTableCell align="left">
+                    {" "}
+                    <Button
+                      variant="contained"
+                      color="primary"
+                      onClick={() => handleAction(row)}
+                    >
+                      View More
+                    </Button>
+                  </StyledTableCell>
+                </StyledTableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        {/* <TableContainer component={Paper}>
           <Table>
             <TableHead>
               <TableRow>
@@ -100,9 +165,9 @@ export default function Files({ user }) {
                   </TableCell>
                 </TableRow>
               ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+            </TableBody> */}
+        {/* </Table>
+        </TableContainer> */}
       </Box>
     </div>
   );
